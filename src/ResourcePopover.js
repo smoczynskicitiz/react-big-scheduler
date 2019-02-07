3727bd4d9d47f510b9079fa905fa9ac281585790
smoczynskicitiz <l.smoczynski@citiz.fr>
2019-02-07 14:17:10 +0100

    Add ResourcePopover + add example in CustomPopoverStyle

---
+++ src/ResourcePopover.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import { PropTypes } from "prop-types";
+import Col from "antd/lib/col";
+import Row from "antd/lib/row";
+import "antd/lib/grid/style/index.css";
+
+class ResourcePopover extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static propTypes = {
+    schedulerData: PropTypes.object.isRequired,
+    resourceItem: PropTypes.object.isRequired,
+    title: PropTypes.string.isRequired,
+    statusColor: PropTypes.string.isRequired,
+    subtitleGetter: PropTypes.func,
+    resourceItemPopoverTemplateResolver: PropTypes.func
+  };
+
+  render() {
+    const {
+      schedulerData,
+      resourceItem,
+      title,
+      statusColor,
+      subtitleGetter,
+      resourceItemPopoverTemplateResolver
+    } = this.props;
+
+    if (resourceItemPopoverTemplateResolver != undefined) {
+      // TODO
+      return resourceItemPopoverTemplateResolver(
+        schedulerData,
+        resourceItem,
+        title,
+        statusColor
+      );
+    } else {
+      let subtitleRow = <div />;
+      if (subtitleGetter !== undefined) {
+        let subtitle = subtitleGetter(schedulerData, eventItem);
+        if (subtitle != undefined) {
+          subtitleRow = (
+            <Row type="flex" align="middle">
+              <Col span={2}>
+                <div />
+              </Col>
+              <Col span={22} className="overflow-text">
+                <span className="header2-text" title={subtitle}>
+                  {subtitle}
+                </span>
+              </Col>
+            </Row>
+          );
+        }
+      }
+
+      return (
+        <div style={{ width: "300px" }}>
+          <Row type="flex" align="middle">
+            <Col span={2}>
+              <div
+                className="status-dot"
+                style={{ backgroundColor: statusColor }}
+              />
+            </Col>
+            <Col span={22} className="overflow-text">
+              <span className="header2-text" title={title}>
+                {title}
+              </span>
+            </Col>
+          </Row>
+          {subtitleRow}
+          <Row type="flex" align="middle">
+            <Col span={2}>
+              <div />
+            </Col>
+          </Row>
+        </div>
+      );
+    }
+  }
+}
+
+export default ResourcePopover;
--- src/ResourceView.js
+++ src/ResourceView.js
@@ -1,61 +1,101 @@
-import React, {Component} from 'react'
-import {PropTypes} from 'prop-types'
+import React, { Component } from "react";
+import { PropTypes } from "prop-types";
+import ResourcePopover from "./ResourcePopover";
+import Popover from "antd/lib/popover";
 
 class ResourceView extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-    }
+  static propTypes = {
+    schedulerData: PropTypes.object.isRequired,
+    contentScrollbarHeight: PropTypes.number.isRequired,
+    slotClickedFunc: PropTypes.func,
+    slotItemTemplateResolver: PropTypes.func
+  };
 
-    static propTypes = {
-        schedulerData: PropTypes.object.isRequired,
-        contentScrollbarHeight: PropTypes.number.isRequired,
-        slotClickedFunc: PropTypes.func,
-        slotItemTemplateResolver: PropTypes.func
-    }
+  render() {
+    const {
+      schedulerData,
+      contentScrollbarHeight,
+      slotClickedFunc,
+      slotItemTemplateResolver
+    } = this.props;
+    const { renderData } = schedulerData;
 
-    render() {
+    let width = schedulerData.getResourceTableWidth() - 2;
+    let paddingBottom = contentScrollbarHeight;
+    let resourceList = renderData.map(item => {
+      let content = (
+        <ResourcePopover
+          {...this.props}
+          resourceItem={item}
+          title={"title"}
+          startTime={new Date()}
+          endTime={new Date()}
+          statusColor={"#AA0000"}
+        />
+      );
 
-        const {schedulerData, contentScrollbarHeight, slotClickedFunc, slotItemTemplateResolver} = this.props;
-        const {renderData} = schedulerData;
+      let a =
+        slotClickedFunc != undefined ? (
+          <a
+            onClick={() => {
+              slotClickedFunc(schedulerData, item);
+            }}
+          >
+            <span className="expander-space" />
+            {item.slotName}
+          </a>
+        ) : (
+          <span>
+            <span className="expander-space" />
+            <span>{item.slotName}</span>
+          </span>
+        );
+      let slotItem = (
+        <div
+          title={item.slotName}
+          className="overflow-text header2-text"
+          style={{ textAlign: "left" }}
+        >
+          {a}
+        </div>
+      );
+      if (!!slotItemTemplateResolver) {
+        let temp = slotItemTemplateResolver(
+          schedulerData,
+          item,
+          slotClickedFunc,
+          width,
+          "overflow-text header2-text"
+        );
+        if (!!temp) slotItem = temp;
+      }
 
-        let width = schedulerData.getResourceTableWidth() - 2;
-        let paddingBottom = contentScrollbarHeight;
-        let resourceList = renderData.map((item) => {
-            let a = slotClickedFunc != undefined ? <a onClick={() => {
-                slotClickedFunc(schedulerData, item);
-            }}><span className="expander-space"></span>{item.slotName}</a>
-                : <span><span className="expander-space"></span><span>{item.slotName}</span></span>;
-            let slotItem = (
-                <div title={item.slotName} className="overflow-text header2-text" style={{textAlign: "left"}}>
-                    {a}
-                </div>
-            );
-            if(!!slotItemTemplateResolver) {
-                let temp = slotItemTemplateResolver(schedulerData, item, slotClickedFunc, width, "overflow-text header2-text");
-                if(!!temp)
-                    slotItem = temp;
-            }
+      return (
+        <Popover placement="bottomLeft" content={content} trigger="hover">
+          <tr key={item.slotId}>
+            <td
+              data-resource-id={item.slotId}
+              style={{ height: item.rowHeight }}
+            >
+              {slotItem}
+            </td>
+          </tr>
+        </Popover>
+      );
+    });
 
-            return (
-                <tr key={item.slotId}>
-                    <td data-resource-id={item.slotId} style={{height: item.rowHeight}}>
-                        {slotItem}
-                    </td>
-                </tr>
-            );
-        });
-
-        return (
-            <div style={{paddingBottom: paddingBottom}}>
-                <table className="resource-table">
-                    <tbody>
-                        {resourceList}
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
+    return (
+      <div style={{ paddingBottom: paddingBottom }}>
+        <table className="resource-table">
+          <tbody>{resourceList}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
-export default ResourceView
+export default ResourceView;
